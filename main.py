106bd165799d91df039ae296278d1bd50c9fb79d
hareem284 <sehrishandaleeb06@gmail.com>
2025-04-17 16:38:19 +0500

    intiale commit

--- main.py
+++ main.py
@@ -26,12 +26,10 @@ def truncate_text(caption,no_of_Words):
 
 #derining a function
 def generate_text(prompt, model="gpt2",max_new_tokens=50):
+    API_URL = f"https://api-inference.huggingface.co/models/{model}"
     print("generating text with prompt {} ".format(prompt))
     payload={"inputs":prompt,
               "parameters":{"max_new_tokens":max_new_tokens}}
-    headers = {
-    "Authorization": f"Bearer {HF_API_KEY}"
-    }
     response=requests.post(API_URL,headers=headers,json=payload,files=None )
     if response.status_code==200:
         print("this was sucsesful")
@@ -44,13 +42,15 @@ def generate_text(prompt, model="gpt2",max_new_tokens=50):
         generated_caption = result[0].get("generated_text", " ")
         return generated_caption
 
-
+#defing a function which will have the summary
 
 
 
 
 
     
+
+
 #defining a function
 def caption_image():
 #Loads the local image file "testing.jpg" and sends it to the Hugging Face Inference API for captioning.
@@ -83,19 +83,28 @@ if __name__=="__main__":
    basic_caption=caption_image()
    while True:
         print_menue()
-        choice=input("enter your choice")
+        choice=input("enter your choice").strip()
         if choice=='1':
             #i need to take 5 words from the caption_image
             caption=truncate_text(basic_caption,5)
             print(caption)
+            break
         elif choice=='2':
             print("i need to make a description of the picture of atleast 30 words")
             promt_text=f"Expand the basic caption to 30 words: {basic_caption}"
-            generate_text=generate_text(promt_text,max_new_tokens=40)
-            print(generate_text)
-            generate_text=generate_text(promt_text, model="gpt2",max_new_tokens=40)
-            if generate_text!=None:
-               caption=truncate_text(generate_text,30)
-               print(caption)
+            gen_text=generate_text(promt_text, model="gpt2",max_new_tokens=40)
+            generate_caption=truncate_text(gen_text,30)
+            print(generate_caption)
+            break
+        #doing choice 3
+        elif choice=='3':
+           print("so you have entered choice 3")
+           prompt3=f"summarizing the content of the caption to a summary of exactly 50 words :{basic_caption}"
+           gen_summary=generate_text(prompt3, model="gpt2",max_new_tokens=60)
+           generate_cap=truncate_text(gen_summary,50)
+           print(generate_cap)
+           break
+
+            
     
 
